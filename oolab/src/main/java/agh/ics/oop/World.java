aa9e11bff88e4335fdfff58755108245fa67e063
KubaBiz <1kubi1@wp.pl>
2022-10-18 19:00:05 +0200

    Second try

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,18 +1,40 @@
 package agh.ics.oop;
 
 public class World {
-    public static void run(String[] tab){
-        System.out.println("Zwierzak idzie do przodu");
+    public static void change(String[] tab, Direction[] kierunki){
         int size = tab.length;
-        for (int i = 0;i<size-1;i++){
-            System.out.print(tab[i]+", ");
+        for (int i = 0;i<size;i++) {
+            switch (tab[i]) {
+                case "f" -> kierunki[i] = Direction.FORWARD;
+                case "b" -> kierunki[i] = Direction.BACKWARD;
+                case "r" -> kierunki[i] = Direction.RIGHT;
+                case "l" -> kierunki[i] = Direction.LEFT;
+                default -> kierunki[i] = Direction.UNKNOWN;
+            }
         }
-        System.out.println(tab[size-1]);
-
+    }
+    public static void run(Direction[] tab){
+        int size = tab.length;
+        for (int i = 0;i<size;i++) {
+            String message = switch (tab[i]) {
+                case FORWARD -> "Do przodu";
+                case BACKWARD -> "Do tyłu";
+                case LEFT -> "Skręca w lewo";
+                case RIGHT -> "Skręca w prawo";
+                case UNKNOWN -> "Nieznana komenda";
+            };
+            if (i < size - 1) {
+                System.out.println(message + ", ");
+            } else {
+                System.out.println(message);
+            }
+        }
     }
     public static void main(String[] args) {
-        System.out.println("system wystartował");
-        run(args);
-        System.out.print("system zakończył działanie");
+        System.out.println("System START");
+        Direction[] kierunki = new Direction[args.length];
+        change(args, kierunki);
+        run(kierunki);
+        System.out.print("System STOP");
     }
 }
